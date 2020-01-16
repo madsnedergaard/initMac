2fb2a4925873318fa8374c8840dc0e96920ea27d
Mads Nedergaard <nedergaardmads@gmail.com>
2020-01-16 11:54:41 +0100

    deletes old files

--- utils/php.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-echo "<h1>PHP Info</h1><hr>";
-echo "<h2>PHP extensions loaded:</h2>";
-echo '<div style="font-family: monospace">';
-echo implode(', ', get_loaded_extensions(false));
-echo '</div>';
-
-phpinfo();
